8bea7d0d0e5a1af9534a25d0aa3b9e69076b5242
William Salvidge <willsalvidge@googlemail.com>
2021-10-26 17:49:49 +0100

    changed Form divs

--- app/components/Form.js
+++ app/components/Form.js
@@ -29,69 +29,77 @@ function Form() {
   return (
     <>
       <div className="checkbox">
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="buttonA"
-                checked={contextChecked.buttonA}
-                onChange={handleChange}
-                inputProps={{
-                  "aria-label": "primary checkbox"
-                }}
-              />
-            }
-            label="Button A"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="buttonB"
-                checked={contextChecked.buttonB}
-                onChange={handleChange}
-                inputProps={{
-                  "aria-label": "primary checkbox"
-                }}
-              />
-            }
-            label="Button B"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="buttonC"
-                checked={contextChecked.buttonC}
-                onChange={handleChange}
-                inputProps={{
-                  "aria-label": "primary checkbox"
-                }}
-              />
-            }
-            label="Button C"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="buttonD"
-                checked={contextChecked.buttonD}
-                onChange={handleChange}
-                inputProps={{
-                  "aria-label": "primary checkbox"
-                }}
-              />
-            }
-            label="Button D"
-          />
-        </FormGroup>
-        <Link to="/results">
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-          >
-            Submit
-          </Button>
-        </Link>
+        <div className="checkbox-grid">
+          <div className="inline-grid">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="buttonA"
+                  checked={contextChecked.buttonA}
+                  onChange={handleChange}
+                  inputProps={{
+                    "aria-label":
+                      "primary checkbox"
+                  }}
+                />
+              }
+              label="Button A"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="buttonB"
+                  checked={contextChecked.buttonB}
+                  onChange={handleChange}
+                  inputProps={{
+                    "aria-label":
+                      "primary checkbox"
+                  }}
+                />
+              }
+              label="Button B"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="buttonC"
+                  checked={contextChecked.buttonC}
+                  onChange={handleChange}
+                  inputProps={{
+                    "aria-label":
+                      "primary checkbox"
+                  }}
+                />
+              }
+              label="Button C"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="buttonD"
+                  checked={contextChecked.buttonD}
+                  onChange={handleChange}
+                  inputProps={{
+                    "aria-label":
+                      "primary checkbox"
+                  }}
+                />
+              }
+              label="Button D"
+            />
+          </div>
+        </div>
+        <div className="submit-button">
+          <Link to="/results">
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+            >
+              Submit
+            </Button>
+          </Link>
+        </div>
       </div>
     </>
   );
